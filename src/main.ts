fcbe434216e49e588968faffa1f75140649fbc56
Chanoon Na <chanoonna@gmail.com>
2023-02-22 21:45:40 -0800

    Add rules to tsconfig and eslintrc. Add close window conditions to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+// import path from 'path';
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,10 +7,19 @@ const createWindow = () => {
     height: 768,
     webPreferences: {
       nodeIntegration: true
+      // preload: path.join(__dirname, 'preload.js')
     }
   });
-
   win.loadFile('../index.html');
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+};
+
+const closeWindow = () => {
+  if (process.platform !== 'darwin') app.quit();
 };
 
 app.on('ready', createWindow);
+app.on('window-all-closed', closeWindow);
