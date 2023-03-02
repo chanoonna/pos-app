80dbe408744091ecf331ccac82a972b2bec4441c
Chanoon Na <chanoonna@gmail.com>
2023-03-01 20:38:27 -0800

    Create LanguageSetting.tsx for language select control

---
+++ src/renderer/components/menus/LanguageSetting.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import TranslateIcon from '@mui/icons-material/Translate';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
+import { useAppContext } from 'modules/App/AppContextProvider';
+import { Language } from 'SettingsModule/types';
+
+export const LanguageSetting = () => {
+  const [targetRef, setTargetRef] = useState<null | HTMLElement>(null);
+  const { setLanguage } = useAppContext();
+
+  const toggleLanguageMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setTargetRef((prev) => (prev ? null : event.currentTarget));
+  };
+
+  return (
+    <div>
+      <IconButton
+        size="medium"
+        aria-controls={'language-menu'}
+        aria-haspopup="true"
+        aria-expanded={'true'}
+        onClick={toggleLanguageMenu}
+      >
+        <TranslateIcon fontSize="medium" sx={{ color: 'white' }} />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={targetRef}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+        open={Boolean(targetRef)}
+        onClose={toggleLanguageMenu}
+        sx={{ maxHeight: '24rem' }}
+      >
+        {Object.values(Language).map((language, index) => (
+          <MenuItem
+            key={language}
+            onClick={(e) => {
+              setLanguage(language);
+              toggleLanguageMenu(e);
+            }}
+          >
+            {language}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
