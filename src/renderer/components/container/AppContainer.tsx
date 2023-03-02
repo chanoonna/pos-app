2cab6189c1bcdfc95ad8492a2fcbdcdfa2063108
Chanoon Na <chanoonna@gmail.com>
2023-03-01 20:38:59 -0800

    Remove side paddings from AppContainer

--- src/renderer/components/container/AppContainer.tsx
+++ src/renderer/components/container/AppContainer.tsx
@@ -5,7 +5,9 @@ export const AppContainer = ({ children }: { children: ReactNode }) => (
   <Container
     sx={{
       height: '100%',
-      width: '100%'
+      width: '100%',
+      paddingLeft: '0 !important',
+      paddingRight: '0 !important'
     }}
   >
     {children}
