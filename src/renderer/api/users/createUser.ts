dfdb08544e47cc644c2fddfe591f2a51b8b0126d
Chanoon Na <chanoonna@gmail.com>
2023-03-18 20:18:24 -0700

    Create action request 'createUser'

---
+++ src/renderer/api/users/createUser.ts
@@ -0,0 +1,13 @@
+/* ---------------------------------- types --------------------------------- */
+
+import type { User } from 'models/user';
+import type { CreateUserParams } from 'preload/api/users/types';
+
+/* --------------------------------- imports -------------------------------- */
+
+import { request } from 'api/utils';
+
+/* ------------------------------------ - ----------------------------------- */
+
+export const createUser = (params: CreateUserParams) =>
+  request<CreateUserParams, User>({ action: 'createUser', params });
